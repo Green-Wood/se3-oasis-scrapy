faff4244b8e8a014a36d368b4decb879dd748eb9
赵文祺 <1027572886a@gmail.com>
2020-05-16 19:29:11 +0800

    更新关键词获取方法

--- ieee/spiders/utils.py
+++ ieee/spiders/utils.py
@@ -1,18 +1,22 @@
 def get_keywords(paper):
     if 'keywords' not in paper:
         return None
-    res = []
-    for kds in paper['keywords']:
-        if 'kwd' in kds and 'type' in kds and 'Author Keywords' not in kds['type']:
-            res += kds['kwd']
+    kwd = None
+    for obj in paper['keywords']:
+        if obj['type'] == 'INSPEC: Controlled Indexing':
+            kwd = obj['kwd']
+    # res = []
+    # for kds in paper['keywords']:
+    #     if 'kwd' in kds and 'type' in kds and 'Author Keywords' not in kds['type']:
+    #         res += kds['kwd']
     
-    if not res:
-        return None
+    # if not res:
+    #     return None
     
-    tmp = []
-    for kwd in res:
-        if len(kwd) > 15 and ',' in kwd:
-            tmp += [x for x in kwd.split(',')]
-        else:
-            tmp.append(kwd)
-    return tmp
+    # tmp = []
+    # for kwd in res:
+    #     if len(kwd) > 15 and ',' in kwd:
+    #         tmp += [x for x in kwd.split(',')]
+    #     else:
+    #         tmp.append(kwd)
+    return kwd
